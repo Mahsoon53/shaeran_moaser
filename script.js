9ed13dcf92018f01c797ec139fe833e605a603e8
Mahsoon53 <clickhot7@gmail.com>
2025-07-08 14:11:18 +0330

    Create script.js
    افزودن جاوا اسکریپت

---
+++ script.js
@@ -0,0 +1,7 @@
+
+```javascript
+document.getElementById('registration-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('ثبت‌نام با موفقیت انجام شد!');
+});
+```
